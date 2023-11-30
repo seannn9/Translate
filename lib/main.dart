617dbd6f9f6f5dcd02e96126d4c8eb274683b70b
seannn9 <naeskirlu9@gmail.com>
2023-11-30 15:42:50 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:translator/translator.dart';
 
+const List<String> list = <String>['English', 'Japanese', 'Filipino'];
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,19 +16,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String dropdownValue = list.first;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+
         appBar: AppBar(
           title: Text("Translate"),
           centerTitle: true,
           backgroundColor: Colors.grey[900],
         ),
-        body: SafeArea(
+        body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
+              Row(
+                children: [
+                  Expanded(child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter Text"
+                      ),
+                    )
+                  )),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print("Translated");
+                },
+                child: Text(
+                  "Translate"
+                ),
+              ),
             ],
           ),
         )
